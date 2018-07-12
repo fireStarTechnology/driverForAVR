7b0e66408525391ba3ea8b92806c60a467d52a86
hp-PC\hp <shrijith.sharma@faststreamtech.com>
2018-07-12 14:28:14 +0530

    Initial project created for ATmega238

---
+++ AVR/myEmAvrDriverLib/myEmAvrDriverLib/main.c
@@ -0,0 +1,18 @@
+/*
+ * myEmAvrDriverLib.c
+ *
+ * Created: 12 7 12 2018 2:26:29 PM
+ * Author : hp
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
